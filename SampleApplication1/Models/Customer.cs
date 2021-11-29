bf1d18a5b854679cfc91e7a74f16b28b2cf25f1b
Sangbida Roy <sangbida.roy21099@outlook.com>
2021-11-29 18:26:50 +0530

    CRUD Application Initial copy

---
+++ SampleApplication1/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication1.Models
+{
+    public class Customer
+    {
+       [Display(Name = "Customer ID")]
+     public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Contact")]
+        public string Contact { get; set; }
+
+        [Display(Name = "DOB")]
+        //[DataType(DataType.Date)]
+    
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "EmailID")]
+        public string EmailID { get; set; }
+
+    }
+}
---
+++ SampleApplication1/Models/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SampleApplication1.Models;
+
+namespace SampleApplication1.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+    }
+}
